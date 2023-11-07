5aa2d1d782ccccd9771fd980605ab08dd7960e18
Daan Martens <57445185+DaanMartensPXL@users.noreply.github.com>
2023-11-07 21:31:58 +0100

    Update carrousel.routes.js

--- backend/routes/carrousel.routes.js
+++ backend/routes/carrousel.routes.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const carrouselRouter = express.Router();
-const carrouselData = require('./../data/carrousel.json');
+const { S3Client, ListObjectsV2Command } = require('@aws-sdk/client-s3');
 
-carrouselRouter.get('', (req, res) => {
-    res.json(carrouselData);
+const bucketName = process.env.S3_BUCKET_NAME || 'default-bucket-name';
+
+async function getS3ObjectUrls() {
+  const client = new S3Client({ region: "us-east-1" });
+  const command = new ListObjectsV2Command({ Bucket: bucketName });
+
+  try {
+    const result = await client.send(command);
+    return result.Contents.map((obj) => `https://${bucketName}.s3.amazonaws.com/${obj.Key}`);
+  } catch (err) {
+    console.error("Error fetching S3 objects:", err);
+    return [];
+  }
+}
+
+carrouselRouter.get('', async (req, res) => {
+    const imageUrls = await getS3ObjectUrls();
+    res.json(imageUrls.map(url => ({ url })));
 });
-
 
 module.exports = carrouselRouter;
