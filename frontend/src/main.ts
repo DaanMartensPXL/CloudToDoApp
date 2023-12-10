679c01387d2e27459b30009d86f9e1cd355a31fc
Daan Martens <57445185+DaanMartensPXL@users.noreply.github.com>
2023-12-10 17:49:21 +0100

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,21 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+    applicationId: '18ca21dc-873e-474b-9a31-5b566435c0cf',
+    clientToken: 'pub967ec225e38e0785b76094b536e815a1',
+    site: 'datadoghq.com',
+    service: 'frontend',
+    env: 'test',
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 20,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: 'allow',
+});
 
 if (environment.production) {
   enableProdMode();
